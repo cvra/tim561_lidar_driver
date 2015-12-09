bf43859a75f2a72fbe8cc0af4eccb1f3e3d0cf3c
Dorian Konrad <dorian.konrad@gmail.com>
2015-12-09 17:37:53 +0100

    Change the way TCP stream is read. Code first part of datagram decoding

--- datagram.py
+++ datagram.py
@@ -34,97 +34,60 @@ Sick561Datagram = collections.namedtuple("sick561_datagram", ["TypeOfCommand",
                                                                # "TimeInformation",
                                                                # "EventInformation"])
 
-class PacketDecoder():
+def bytes_from_socket(socket):
+    while True:
+        data = socket.recv(256)
+        for byte in data:
+            yield bytes([byte])
+
+def datagrams_from_socket(socket):
     STX = b'\x02'
     ETX = b'\x03'
 
-    def __init__(self):
-        self._stream = b''
+    byte_generator = bytes_from_socket(socket)
 
-    def feed(self, buffer):
-        self._stream += buffer
+    while True:
+        datagram = b''
 
-    def get_datagram(self):
-        '''
-        Yields packets from the current stream.
-        '''
-        while len(self._stream) > 2:
-            start = self._stream.find(self.STX)
-            if start == -1:
+        for byte in byte_generator:
+            if byte == STX:
                 break
 
-            end = self._stream.find(self.ETX, start)
-            if end == -1:
+        for byte in byte_generator:
+            if byte == ETX:
                 break
+            datagram += byte
+        yield datagram
 
-            packet = self._stream[start:end+1]
-            yield packet
-            self._stream = self._stream[end+1:]
+def parse_number(nbr_str):
+    """ decimal numbers are encoded with leading +/- """
+    if b'+' in nbr_str or b'-' in nbr_str:
+        return int(nbr_str)
+    else:
+        return int(nbr_str, 16)
 
+def decode_datagram(datagram):
+    items = datagram.split(b' ')
 
-class DatagramReader():
-    def __init__(self, socket):
-        self._socket = socket
-        self._decoder = PacketDecoder()
-        self._gen = None
+    header = {}
+    header['TypeOfCommand'] = items[0].decode('ascii')
+    if header['TypeOfCommand'] != 'sSN':
+        return None
+    header['Command'] = items[1].decode('ascii')
+    if header['Command'] != 'LMDscandata':
+        return None
+    header['VersionNumber'] = parse_number(items[2])
+    header['DeviceNumber'] = parse_number(items[3])
+    header['SerialNumber'] = items[4].decode('ascii')
+    header['DeviceStatus1'] = parse_number(items[5])
+    header['DeviceStatus2'] = parse_number(items[6])
+    if header['DeviceStatus1'] != 0 or header['DeviceStatus2'] != 0:
+        return None
+    header['TelegramCounter'] = parse_number(items[7])
+    header['TimeSinceStartup'] = parse_number(items[9])
+    header['TimeOfTransmission'] = parse_number(items[10])
+    header['AngularStepWidth'] = parse_number(items[24])
+    header['NumberOfData'] = parse_number(items[25])
 
-    def receive(self):
-        data = self._socket.recv(256)
-        self._decoder.feed(data)
-        return data
+    return header
 
-    def read_next_datagram(self):
-        datagram = None
-        while datagram is None:
-            if not self._gen:
-                self._gen = self._decoder.get_datagram()
-
-            datagram = next(self._gen, None)
-            if datagram == None:
-                self._gen = None
-                self.receive()
-
-        datagram = datagram.lstrip(PacketDecoder.STX)
-        datagram = datagram.rstrip(PacketDecoder.ETX)
-        return datagram
-
-    @staticmethod
-    def parse_number(nbr_str):
-        """ decimal numbers are encoded with leading +/- """
-        if b'+' in nbr_str or b'-' in nbr_str:
-            return int(nbr_str)
-        else:
-            return int(nbr_str, 16)
-
-    @staticmethod
-    def decode_datagram(datagram):
-        items = datagram.split(b' ')
-        # debug(items)
-        # debug(len(items))
-
-        parse_number = DatagramReader.parse_number
-
-        header = {}
-        header['TypeOfCommand'] = items[0].decode('ascii')
-        if header['TypeOfCommand'] != 'sSN':
-            return None
-        header['Command'] = items[1].decode('ascii')
-        if header['Command'] != 'LMDscandata':
-            return None
-        header['VersionNumber'] = parse_number(items[2])
-        header['DeviceNumber'] = parse_number(items[3])
-        header['SerialNumber'] = items[4].decode('ascii')
-        header['DeviceSatus1'] = parse_number(items[5])
-        header['DeviceSatus2'] = parse_number(items[6])
-        if header['DeviceSatus1'] != 0 or header['DeviceSatus2'] != 0:
-            return None
-        header['TelegramCounter'] = parse_number(items[7])
-        header['TimeSinceStartup'] = parse_number(items[9])
-        header['TimeOfTransmission'] = parse_number(items[10])
-        header['AngularStepWidth'] = parse_number(items[24])
-        header['NumberOfData'] = parse_number(items[25])
-
-        return header
-
-        # fmt = '3s11s2HI4H2I5H2I2H5s2fI2H'
-        # return Sick561Datagram._make(unpack(fmt, datagram[0:88]))
